e4e77c48be02eb9e48c3b6bea02e1c2407b0760f
jagoda-b <mirreal@gmail.com>
2023-05-30 14:03:06 +0200

    Stage 2, refactor - cleaning switch

--- Meal Planner (Java)/task/src/mealplanner/Main.java
+++ Meal Planner (Java)/task/src/mealplanner/Main.java
@@ -10,70 +10,66 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         List<Meal> meals = new ArrayList<>();
+        boolean menuFlag = true;
 
-        while (true){
+        while (menuFlag){
             String command = Utility.getInfoFromUser("What would you like to do (add, show, exit)?", scanner);
 
-            if (command.equals("exit")) {
-                System.out.println("Bye!");
-                break;
-            }
 
             switch (command) {
                 case "add" :
-                    Meal.MealBuilder mealBuilder = new Meal.MealBuilder();
-
-                    String mealType = Utility.getInfoFromUser("Which meal do you want to add (breakfast, lunch, dinner)?", scanner);
-                    while (!Utility.validateMealType(mealType)){
-                        mealType =  Utility.getInfoFromUser("Wrong meal category! Choose from: breakfast, lunch, dinner.", scanner);
-                    }
-
-
-                    String mealName = Utility.getInfoFromUser("Input the meal's name: ", scanner);
-                    while (!Utility.validateMealName(mealName)){
-                        mealName =  Utility.getInfoFromUser("Wrong format. Use letters only!", scanner);
-                    }
-
-
-                    String ingredients = Utility.getInfoFromUser("Input the ingredients: ", scanner);
-                    while (!Utility.validateIngredients(ingredients)){
-                        ingredients =  Utility.getInfoFromUser("Wrong format. Use letters only!", scanner);
-                    }
-
-                    Meal newMeal = mealBuilder.addType(mealType).addName(mealName).addIngredients(ingredients).build();
-
-                    meals.add(newMeal);
-                    System.out.println("The meal has been added!");
+                    addCommand(scanner, meals);
                     break;
 
                 case "show" :
-                    System.out.println("");
-                    if (meals.isEmpty()){
-                        System.out.println("No meals saved. Add a meal first.");
-                    }
-
-                    for ( Meal meal : meals) {
-                        System.out.println(meal.toString());
-                    }
-
+                    showCommand(meals);
                     break;
 
-
+                case "exit" :
+                    System.out.println("Bye!");
+                    menuFlag = false;
             }
 
         }
 
-
-
-
-
-
-
-
-
-
         scanner.close();
     }
 
+    private static void addCommand(Scanner scanner, List<Meal> meals) {
+        Meal.MealBuilder mealBuilder = new Meal.MealBuilder();
+
+        String mealType = Utility.getInfoFromUser("Which meal do you want to add (breakfast, lunch, dinner)?", scanner);
+        while (!Utility.validateMealType(mealType)){
+            mealType =  Utility.getInfoFromUser("Wrong meal category! Choose from: breakfast, lunch, dinner.", scanner);
+        }
+
+
+        String mealName = Utility.getInfoFromUser("Input the meal's name: ", scanner);
+        while (!Utility.validateMealName(mealName)){
+            mealName =  Utility.getInfoFromUser("Wrong format. Use letters only!", scanner);
+        }
+
+
+        String ingredients = Utility.getInfoFromUser("Input the ingredients: ", scanner);
+        while (!Utility.validateIngredients(ingredients)){
+            ingredients =  Utility.getInfoFromUser("Wrong format. Use letters only!", scanner);
+        }
+
+        Meal newMeal = mealBuilder.addType(mealType).addName(mealName).addIngredients(ingredients).build();
+
+        meals.add(newMeal);
+        System.out.println("The meal has been added!");
+
+    }
+
+    private static void showCommand(List<Meal> meals) {
+        if (meals.isEmpty()){
+            System.out.println("No meals saved. Add a meal first.");
+        }
+
+        for ( Meal meal : meals) {
+            System.out.println(meal.toString());
+        }
+    }
 
 }

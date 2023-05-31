7dbcbfbf15eff2f9304ce071239b26d38bc9e765
jagoda-b <mirreal@gmail.com>
2023-05-31 11:44:30 +0200

    Stage 3, first take at connection to database

--- Meal Planner (Java)/task/src/mealplanner/Main.java
+++ Meal Planner (Java)/task/src/mealplanner/Main.java
@@ -3,11 +3,32 @@ package mealplanner;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
+import java.sql.*;
 
 
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
+        String DB_URL = "jdbc:postgresql:meals_db";
+        String USER = "postgres";
+        String PASS = "1111";
+
+        Connection connection = DriverManager.getConnection(DB_URL, USER, PASS);
+        connection.setAutoCommit(true);
+
+        Statement statement = connection.createStatement();
+
+
+        ResultSet rs = statement.executeQuery("SELECT * FROM meals");
+        while (rs.next()) {
+            System.out.println("category = " + rs.getString("category"));
+            System.out.println("meal = " + rs.getString("meal"));
+            System.out.println("meal_id = " + rs.getInt("meal_id"));
+        }
+        statement.close();
+        connection.close();
+
+
         Scanner scanner = new Scanner(System.in);
         List<Meal> meals = new ArrayList<>();
         boolean menuFlag = true;
